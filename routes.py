208293fa76a9c021e90a64ccccd9cc066df28878
Ahmet Hakan Beşel <ahmethakan.besel@gmail.com>
2021-12-27 23:11:00 +0300

    Remove unnecessary code

--- routes.py
+++ routes.py
@@ -11,17 +11,6 @@ from models import Course
 router = APIRouter()
 
 
-@router.get("/")
-def read_root():
-    return {"Hello": "World"} \
- \
-           @ router.get("/items/{item_id}")
-
-
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
 @router.get("/grade", tags=['Grade'])
 def read_root():
     return [{'grade': 9}, {'grade': 10}, {'grade': 11}, {'grade': 12}]
