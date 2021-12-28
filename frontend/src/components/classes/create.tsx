e8b1786aafa7903ed7266701ba71b583a7522810
Ahmet Hakan Beşel <ahmethakan.besel@gmail.com>
2021-12-28 07:56:47 +0300

    Add select for class grade

--- frontend/src/components/classes/create.tsx
+++ frontend/src/components/classes/create.tsx
@@ -75,7 +75,26 @@ export const ClassCreate: React.FC<IResourceComponentsProps> = () => {
                         },
                     ]}
                 >
-                    <Input/>
+                    <Select
+                        options={[
+                            {
+                                label: "9",
+                                value: 9,
+                            },
+                            {
+                                label: "10",
+                                value: 10,
+                            },
+                            {
+                                label: "11",
+                                value: 11,
+                            },
+                            {
+                                label: "12",
+                                value: 12,
+                            },
+                        ]}
+                    />
                 </Form.Item>
                 <Form.Item
                     label="Semester"
